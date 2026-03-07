fc55821e590ab84929a97b15753414d9ff08888a
Sudipto <sbose562@gmail.com>
2026-03-07 15:36:42 +0530

    Updated

--- Repository/PurchaseRepository.cs
+++ Repository/PurchaseRepository.cs
@@ -22,10 +22,10 @@ namespace inventoryApiDotnet.Repository
             if (prop != null)
             {
                 // order dynamically by transactionDateTime descending
-                var query = DbSet
-                            .Include(y => y.SerialNumbers
-                                .Where(s => s.isActive))
-                            .OrderByDescending(e => EF.Property<DateTime?>(e, prop.Name));
+                  var query = DbSet
+                            .Include(y => y.SerialNumbers)
+                            .OrderByDescending(e => EF.Property<DateTime?>(e, prop.Name))
+                            .Where(x => x.isActive);
                 return await query.Skip(skip).Take(pageSize).ToListAsync();
             }
 
